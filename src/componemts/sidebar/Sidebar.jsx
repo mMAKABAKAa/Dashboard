e18679dc1f3ac8167e946d45d6169ffed89b1a8d
farmerlillian <pinwenmu@gmail.com>
2024-01-11 22:44:54 +1300

    initial commit

---
+++ src/componemts/sidebar/Sidebar.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import Logo from '../../imgs/logo.png';
+import './Sidebar.css';
+
+import {UilSignOutAlt} from '@iconscout/react-unicons';
+import {SidebarData} from '../../Data/Data.js';
+const Sidebar = () => {
+
+    const [selected,setSelected]=useState(0);
+  return (
+   <div className="Sidebar">
+    {/*logo */}
+    <div className="logo">
+        <img src={Logo} alt="" />
+        <span>Sh<span>o</span>p</span>
+    </div>
+    {/* menu */}
+    <div className="menu">
+        {SidebarData.map((item,index)=>{
+            return(
+                <div className={selected===index?"menuItem active":"menuItem"}
+                key={index}
+                onClick={()=>setSelected(index)}>
+                    <item.icon/>
+                    <span>{item.heading}</span>
+                </div>
+            )
+        })}
+        <div className="menuItem">
+            <UilSignOutAlt/>
+        </div>
+        </div>
+        
+    </div>
+   
+  )
+}
+
+export default Sidebar
